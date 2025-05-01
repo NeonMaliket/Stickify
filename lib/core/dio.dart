d84b2a7d0281b741d4a70b0ada967c1d77280c48
Afterlife <YaroslavKulpin@gmail.com>
2025-05-01 22:55:31 +0300

    trigger actions

--- lib/core/dio.dart
+++ lib/core/dio.dart
@@ -3,6 +3,7 @@ import 'package:stickify/core/logger.dart';
 
 final dio = Dio();
 
+
 String serverHost() {
   final url  = const String.fromEnvironment('STICKIFY_API', defaultValue: "http://10.0.2.2:8080");
   logger.i('URL: $url');
